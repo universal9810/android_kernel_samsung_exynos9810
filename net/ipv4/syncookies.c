9c5d64db4eb7c75912b874e794d160f695e34c38
Maciej Żenczykowski <maze@google.com>
2020-05-15 09:20:37 -0700

    ANDROID: namespace'ify tcp_default_init_rwnd implementation
    Not needed in 4.19+ since this Android specific sysctl was
    not included in later kernels.
    
    Test: via uml net tests with namespaces enabled
    Bug: 149894399
    Bug: 166680775
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Change-Id: I2ad1dbc977d40ee260bde23c6ed32f2706082660
    (cherry picked with simple conflict resolution fixups from
     android-4.14-stable commit 08870bd1a24fc7f3ae4ff30bc7e64c09edd931d4,
     retested via:
       ARCH=um SUBARCH=x86_64 /aosp-tests/net/test/run_net_test.sh --builder all_tests.py
     with patch to make adb detection, ie. HasEstablishedTcpSessionOnPort() always true)

--- net/ipv4/syncookies.c
+++ net/ipv4/syncookies.c
@@ -443,7 +443,7 @@ struct sock *cookie_v4_check(struct sock *sk, struct sk_buff *skb)
 				       ireq->wscale_ok, &rcv_wscale,
 				       dst_metric(&rt->dst, RTAX_INITRWND), sk);
 #else
-	tcp_select_initial_window(tcp_full_space(sk), req->mss,
+	tcp_select_initial_window(sock_net(sk), tcp_full_space(sk), req->mss,
 				  &req->rsk_rcv_wnd, &req->rsk_window_clamp,
 				  ireq->wscale_ok, &rcv_wscale,
 				  dst_metric(&rt->dst, RTAX_INITRWND));
--- net/ipv6/syncookies.c
+++ net/ipv6/syncookies.c
@@ -276,7 +276,7 @@ struct sock *cookie_v6_check(struct sock *sk, struct sk_buff *skb)
 				       ireq->wscale_ok, &rcv_wscale,
 				       dst_metric(dst, RTAX_INITRWND), sk);
 #else
-	tcp_select_initial_window(tcp_full_space(sk), req->mss,
+	tcp_select_initial_window(sock_net(sk), tcp_full_space(sk), req->mss,
 				  &req->rsk_rcv_wnd, &req->rsk_window_clamp,
 				  ireq->wscale_ok, &rcv_wscale,
 				  dst_metric(dst, RTAX_INITRWND));
