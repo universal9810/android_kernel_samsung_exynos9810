ff206106af8280366b536293fca77de8671b700f
ThunderStorms21th nalas <pinakastorm@gmail.com>
2022-01-04 20:48:45 +0400

    drivers: decon: Disable tracing/logging
    org commit : diepquynh/android_kernel_samsung_universal8895@026047f

--- drivers/video/fbdev/exynos/dpu_9810/decon_core.c
+++ drivers/video/fbdev/exynos/dpu_9810/decon_core.c
@@ -48,11 +48,11 @@
 #include "dpp.h"
 #include "displayport.h"
 
-int decon_log_level = 6;
+int decon_log_level = 0;
 module_param(decon_log_level, int, 0644);
-int dpu_bts_log_level = 6;
+int dpu_bts_log_level = 0;
 module_param(dpu_bts_log_level, int, 0644);
-int win_update_log_level = 6;
+int win_update_log_level = 0;
 module_param(win_update_log_level, int, 0644);
 int decon_systrace_enable;
 static int decon2_event_count;
